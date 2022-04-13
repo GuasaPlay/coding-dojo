4a877dc0117e641a3275ed8901889674a095d9a0
Oscar Calle <gameplaysoscar@gmail.com>
2022-04-12 19:31:21 -0500

    Added console log for print result

--- 23-desafios-con-arreglos/index.js
+++ 23-desafios-con-arreglos/index.js
@@ -1,16 +1,24 @@
+// 1. Siempre hambriento
 function alwaysHungry(arr) {
 	let foodCount = 0;
 	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] == 'food') {
-			console.log('yummy');
+		if (arr[i] == 'comida') {
+			console.log('delicioso');
 			foodCount++;
 		}
 	}
 	if (foodCount == 0) {
-		console.log("I'm hungry");
+		console.log('Tengo hambre');
 	}
 }
 
+// esto debería mostrar "delicioso, "delicioso"
+alwaysHungry([3.14, 'comida', 'pastel', true, 'comida']);
+console.log('******');
+// esto debería mostrar "Tengo hambre"
+alwaysHungry([4, 1, 5, 7, 2]);
+console.log('-----------------------------');
+// 2. Filtro paso alto
 function highPass(arr, cutoff) {
 	const filteredArr = [];
 	for (let i = 0; i < arr.length; i++) {
@@ -21,7 +29,10 @@ function highPass(arr, cutoff) {
 	return filteredArr;
 }
 
-// betterThanAverage
+var result = highPass([6, 8, 3, 10, -2, 5, 9], 5);
+console.log(result); // esperamos de vuelta [6, 8, 10, 9]
+console.log('-----------------------------');
+// 3. Mejor que el promedio
 function betterThanAverage(arr) {
 	let sum = 0;
 
@@ -40,9 +51,9 @@ function betterThanAverage(arr) {
 	return count;
 }
 var result = betterThanAverage([6, 8, 3, 10, -2, 5, 9]);
-console.log(result);
-
-// reverse
+console.log(result); // esperamos 4 de vuelta
+console.log('-----------------------------');
+// 4. Arreglo invertido
 function reverse(arr) {
 	var left = 0;
 	var right = arr.length - 1;
@@ -58,9 +69,9 @@ function reverse(arr) {
 }
 
 var result = reverse(['a', 'b', 'c', 'd', 'e']);
-console.log(result);
-
-// fibonacciArray
+console.log(result); // esperamos de vuelta ["e", "d", "c", "b", "a"]
+console.log('-----------------------------');
+// 5. Arreglo de Fibonacci
 function fibonacciArray(n) {
 	let fibArr = [0, 1];
 	while (fibArr.length < n) {
@@ -71,4 +82,4 @@ function fibonacciArray(n) {
 	return fibArr;
 }
 var result = fibonacciArray(10);
-console.log(result);
+console.log(result); // esperamos de vuelta[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
